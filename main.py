091110af9f4e0afc0ab092a2c74af5ba76893597
Ivan Dorofeev <axxel123@rambler.ru>
2022-07-02 08:00:56 +0300

    ready

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import django
 
@@ -8,5 +9,21 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Все карточки пропусков:', Passcard.objects.all())
+
+    rand_number = random.randint(0, len(Passcard.objects.all()))
+    some_passcard = Passcard.objects.all()[rand_number]
+    print('owner_name:', some_passcard.owner_name)
+    print('owner_name:', some_passcard.passcode)
+    print('owner_name:', some_passcard.created_at)
+    print('owner_name:', some_passcard.is_active)
+
+    active_cards = []
+    for card in Passcard.objects.all():
+        if card.is_active:
+            active_cards.append(card)
+    print('Активных пропусков: ', len(active_cards))
+
+    active_cards2 = Passcard.objects.filter(is_active=True)
+    print('Активных пропусков: ', len(active_cards2))
